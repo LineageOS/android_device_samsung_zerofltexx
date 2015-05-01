6eff8ecddf66e76f3f960850530180a2f48147a5
Brandon McAnsh <brandon.mcansh@gmail.com>
2015-05-01 15:09:04 -0400

    zerofltemtr: Update fingerprint and build description

--- cm.mk
+++ cm.mk
@@ -13,8 +13,8 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=zerofltemtr \
     TARGET_DEVICE=zerofltemtr \
-    BUILD_FINGERPRINT="samsung/zerofltemtr/zerofltemtr:5.0.2/LRX22G/G920T1UVU1AOCF:user/release-keys" \
-    PRIVATE_BUILD_DESC="zerofltemtr-user 5.0.2 LRX22G G920T1UVU1AOCF release-keys"
+    BUILD_FINGERPRINT="samsung/zerofltemtr/zerofltemtr:5.0.2/LRX22G/G920T1UVU1AOCH:user/release-keys" \
+    PRIVATE_BUILD_DESC="zerofltemtr-user 5.0.2 LRX22G G920T1UVU1AOCH release-keys"
 
 PRODUCT_NAME := cm_zerofltemtr
 PRODUCT_DEVICE := zerofltemtr
