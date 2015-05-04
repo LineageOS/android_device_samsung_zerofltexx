80b182f5551e3701c5c8b7a83ebe26281c87db7f
bmc08gt <brandon.mcansh@gmail.com>
2015-05-04 23:00:00 +0200

    zerofltemtr: Compile kernel with tmo defconfig
    Signed-off-by: bmc08gt <brandon.mcansh@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -27,4 +27,5 @@ include device/samsung/zeroflte-common/BoardConfigCommon.mk
 TARGET_OTA_ASSERT_DEVICE := zerofltemtr,zeroflte
 
 # Kernel
+TARGET_KERNEL_CONFIG := exynos7420-zeroflte_tmo_defconfig
 TARGET_PREBUILT_KERNEL := device/samsung/zerofltemtr/kernel
