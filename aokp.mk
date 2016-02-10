be0e071ca1b4179dfffa3a982a84d8fade0f6f19
Brandon McAnsh <brandon.mcansh@gmail.com>
2016-02-10 11:40:17 -0500

    Overrive PRODUCT_NAME and TARGET_NAME to display S6
    Signed-off-by: Brandon McAnsh <brandon.mcansh@gmail.com>

--- aokp.mk
+++ aokp.mk
@@ -11,9 +11,8 @@ $(call inherit-product, vendor/aokp/configs/nfc_enhanced.mk)
 $(call inherit-product, vendor/aokp/configs/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=zerofltexx \
-    TARGET_DEVICE=zeroflte \
-    PRODUCT_DEVICE=zeroflte \
+    PRODUCT_NAME=S6 \
+    TARGET_DEVICE=S6 \
     BUILD_FINGERPRINT="samsung/zerofltexx/zeroflte:5.1.1/LMY47X/G920FXXU2COH2:user/release-keys" \
     PRIVATE_BUILD_DESC="zerofltexx-user 5.1.1 LMY47X G920FXXU2COH2 release-keys"
 
