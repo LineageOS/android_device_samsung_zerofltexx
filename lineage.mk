8aca623199a6c74627087c313809dfd50e922354
Brandon McAnsh <brandon.mcansh@gmail.com>
2017-01-08 00:54:53 -0500

    zerofltexx: Rebrand as LineageOS
    Change-Id: I45031a98597200a8056272f1f87c6a60acab4ed7
    Signed-off-by: Brandon McAnsh <brandon.mcansh@gmail.com>

--- lineage.mk
+++ lineage.mk
@@ -1,4 +1,4 @@
-# Initialise device config
+# Initialize device config
 $(call inherit-product, device/samsung/zerofltexx/full_zerofltexx.mk)
 
 TARGET_SCREEN_HEIGHT := 2560
@@ -7,12 +7,12 @@ TARGET_SCREEN_WIDTH := 1440
 # Enhanced NFC
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
-# Inherit some common CM stuff.
+# Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="Galaxy S6" \
     TARGET_DEVICE="Galaxy S6"
 
-PRODUCT_NAME := cm_zerofltexx
+PRODUCT_NAME := lineage_zerofltexx
 PRODUCT_DEVICE := zerofltexx
