3374ebf86567744ad58779c8cd5532baeac61267
Brandon McAnsh <brandon.mcansh@gmail.com>
2017-01-09 18:58:07 -0500

    Rebrand for LineageOS
    Change-Id: I4f959f79d115768ad36d37e0cfffeaf42d7e95e3
    Signed-off-by: Brandon McAnsh <brandon.mcansh@gmail.com>

--- lineage.mk
+++ lineage.mk
@@ -7,12 +7,12 @@ TARGET_SCREEN_WIDTH := 1440
 # Enhanced NFC
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
-# Inherit some common CM stuff.
+# Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="Galaxy S6" \
     TARGET_DEVICE="Galaxy S6"
 
-PRODUCT_NAME := cm_zerofltexx
+PRODUCT_NAME := lineage_zerofltexx
 PRODUCT_DEVICE := zerofltexx
