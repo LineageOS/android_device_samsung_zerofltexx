a055c425e24f4fd20cf76c00aafeb41a08937614
Usaamah Patel <ussyp2015@gmail.com>
2018-01-21 15:21:21 +0000

    zero: vendor/cm -> vendor/lineage
    Change-Id: Ibf6a35d293ac31117f1e05f58a7bf4d6408c72e0
    Signed-off-by: Usaamah Patel <ussyp2015@gmail.com>

--- lineage.mk
+++ lineage.mk
@@ -5,7 +5,7 @@ TARGET_SCREEN_HEIGHT := 2560
 TARGET_SCREEN_WIDTH := 1440
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="zerofltexx" \
